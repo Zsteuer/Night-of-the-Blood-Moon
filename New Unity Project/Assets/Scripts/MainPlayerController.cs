348d33f62041b18c60161ef8f149ba24d209f661
Jason Veloz <42975494+JasonVeloz2020@users.noreply.github.com>
2019-02-18 17:46:49 +0100

    Fixed Jumping :100:

--- New Unity Project/Assets/Scripts/MainPlayerController.cs
+++ New Unity Project/Assets/Scripts/MainPlayerController.cs
@@ -6,15 +6,29 @@ public class MainPlayerController : MonoBehaviour
 {
     public float speed;
     public float jumpSpeed;
-    private Rigidbody2D myRigidBody; 
+    public float isGroundedDistance;
+    public LayerMask excludePlayer; 
+    private Rigidbody2D myRigidBody;
+    Transform tagGround;
+
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        tagGround = GameObject.Find(this.name + "/Ground").transform;
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Move();
+        if (isGrounded())
+        {
+            Jump();
+        }
+    }
+    void Move()
     {
         Vector2 movement = Vector2.zero;
         if (Input.GetKey(KeyCode.A))
@@ -22,19 +36,33 @@ public class MainPlayerController : MonoBehaviour
             //move left
             movement.x = -speed;
         }
-        if(Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             //move right
             movement.x = speed;
         }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            //jump
-            movement.y = jumpSpeed;
-        }
-
-
         myRigidBody.velocity = movement;
     }
-    
+
+    void Jump()
+    {
+        Vector2 jumpMovement = myRigidBody.velocity;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //jump
+            jumpMovement.y = jumpSpeed;
+        }
+        myRigidBody.velocity = jumpMovement;
+    }
+    bool isGrounded()
+    {
+        Vector2 origin = transform.position;
+        Vector2 direction = Vector2.down;
+        if (Physics2D.Linecast(transform.position, tagGround.position, excludePlayer))
+        {
+            return true;
+        } else
+            return false;
+        
+    }
 }
