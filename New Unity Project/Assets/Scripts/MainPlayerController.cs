341509ec1b153e7626cec09bcb466bcda795aa2e
Jason Veloz <42975494+JasonVeloz2020@users.noreply.github.com>
2019-02-18 16:33:44 +0100

    Added MainCharacterController

---
+++ New Unity Project/Assets/Scripts/MainPlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidBody; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            //move left
+            movement.x = -speed;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            //move right
+            movement.x = speed;
+        }
+        
+    }
+}
