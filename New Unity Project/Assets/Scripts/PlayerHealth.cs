dbdd30a973a47523d77c6907b8f50af3b01946d7
Zach Steuer <zsteuer@oberlin.edu>
2019-02-19 11:28:37 +0100

    I hope this works

---
+++ New Unity Project/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int lives;
+    public int maxLives;
+    private GameObject healthPanel;
+    private UIHealthPanel myUIHealthPanel;
+   // public LayerMask enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthPanel = GameObject.Find("HealthPanel");
+        myUIHealthPanel = healthPanel.GetComponent<UIHealthPanel>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myUIHealthPanel.SetLives(maxLives, lives);
+    }
+}
