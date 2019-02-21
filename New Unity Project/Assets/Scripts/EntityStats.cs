0fd3088cf2afaf7fde4687aa5e1aa827eadbc1df
Jason Veloz <42975494+JasonVeloz2020@users.noreply.github.com>
2019-02-21 10:17:57 +0100

    Added EntityStats

---
+++ New Unity Project/Assets/Scripts/EntityStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityStats : MonoBehaviour
+{
+    public float Strength = 1.0f;
+    public float Agility = 1.0f;
+    public float Stamina = 1.0f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- New Unity Project/Assets/Scripts/PlayerSwordAttack.cs
+++ New Unity Project/Assets/Scripts/PlayerSwordAttack.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSwordAttack :  
+public class PlayerSwordAttack : EnemyMovement 
     {
     public SpriteRenderer mySpriteRenderer;
+    public Animator myAnimator;
     public float swordDamageDone = 1.0f;
     // Start is called before the first frame update
     void Start()
@@ -15,34 +16,38 @@ public class PlayerSwordAttack :
     // Update is called once per frame
     void Update()
     {
-        void SwordAttack()
-        {
-            if (Input.GetKeyDown(KeyCode.J))
-            {
-                if (mySpriteRenderer.flipX == true)
-                {
-                    RaycastHit2D swordHit = Physics2D.Raycast(transform.position, Vector2.left);
-                    if (swordHit.collider.name == "Enemy")
-                    {
-                        var myScript
-                        DamageDone(swordDamageDone);
-                    }
-
-                }
-                else if (mySpriteRenderer.flipX == false)
-                {
-                    RaycastHit2D swordHit = Physics2D.Raycast(transform.position, Vector2.right);
-                    if (swordHit.collider != null)
-                    {
-                        DamageDone(swordDamageDone);
-                    }
-                }
-            }
-
-        }
+        SwordAttack();
     }
 
-        void DamageDone(int swordDamageDone)
+    void SwordAttack()
+    {
+        myAnimator.ResetTrigger("IsAttacking");
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            if (mySpriteRenderer.flipX == true)
+            {
+                RaycastHit2D swordHit = Physics2D.Raycast(transform.position, Vector2.left);
+                if (swordHit.collider.tag == "Enemy")
+                {
+                    DamageDone(swordDamageDone);
+                    myAnimator.SetTrigger("IsAttacking");
+                }
+
+            }
+            else if (mySpriteRenderer.flipX == false)
+            {
+                RaycastHit2D swordHit = Physics2D.Raycast(transform.position, Vector2.right);
+                if (swordHit.collider.tag == "Enemy")
+                {
+                    DamageDone(swordDamageDone);
+                    myAnimator.SetTrigger("IsAttacking");
+                }
+            }
+        }
+
+    }
+
+    void DamageDone(int swordDamageDone)
     {
         
     }
