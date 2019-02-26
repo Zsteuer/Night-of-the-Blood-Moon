8c8b8cec7db99939043f4122882359d87b261628
Jason Veloz <42975494+JasonVeloz2020@users.noreply.github.com>
2019-02-26 17:33:05 +0100

    Fixed UseDoor

--- New Unity Project/Assets/Scripts/UseDoor.cs
+++ New Unity Project/Assets/Scripts/UseDoor.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class UseDoor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public string nextLevelName;
+    private bool colliding = false;
     void Update()
     {
-        
+    EnterDoor();
+    }
+
+    void EnterDoor()
+    {
+    while (colliding)
+    {
+        if (Input.GetKeyDown(KeyCode.W)){
+            SceneManager.LoadScene(nextLevelName);
+        }
+    }     
+    }
+    private void OnCollisionEnter2D(Collider2D Player) 
+    {
+        colliding = true;
+      }
+
+    private void OnCollisionExit2D(Collider2D Player)
+    {
+        colliding = false;
     }
 }
