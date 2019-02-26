42cb5f4519ec017446a46cd6a4e66fa0be996aa9
Jason Veloz <42975494+JasonVeloz2020@users.noreply.github.com>
2019-02-26 12:09:11 +0100

    Added TextTrigger and Interactables script

---
+++ New Unity Project/Assets/Scripts/Interactable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public GameObject uiObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.gameObject.layer == 8)
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("Wait");
+            print("yep");
+        }
+    }
+        IEnumerator Wait()
+        {
+            yield return new WaitForSeconds(5);
+            Destroy(uiObject);
+            Destroy(gameObject);
+        }
+    }
+
+
